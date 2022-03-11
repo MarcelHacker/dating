64bbfe8a4608ceb8bdf82058de839644df723941
MarcelHacker <marcelhacker@dev.com>
2022-03-11 18:13:31 +0100

    Add buttons in LoveCard

--- src/components/LoveCard.vue
+++ src/components/LoveCard.vue
@@ -11,9 +11,28 @@
     </el-carousel>
     
   </div>
+
+  <el-row>
+    <el-button :icon="Search" circle></el-button>
+    <el-button type="primary" :icon="Edit" circle></el-button>
+    <el-button type="success" :icon="Check" circle></el-button>
+    <el-button type="info" :icon="Message" circle></el-button>
+    <el-button type="warning" :icon="Star" circle></el-button>
+    <el-button type="danger" :icon="Delete" circle></el-button>
+  </el-row>
 </template>
 
+
+
 <script lang="ts">
+import {
+  Search,
+  Edit,
+  Check,
+  Message,
+  Star,
+  Delete,
+} from '@element-plus/icons-vue'
 
 export default{
    data() {
